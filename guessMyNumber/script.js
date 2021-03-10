5a7641c4e15e96e2bf8f4020e36c8db74a4c20f3
Natalie Acevedo <natalieacevedo25@gmail.com>
2021-03-10 10:54:15 +0100

    creation of folder for dom manipulation

---
+++ guessMyNumber/script.js
@@ -0,0 +1,10 @@
+'use strict';
+
+console.log(document.querySelector('.message').textContent);// selecciona el elemento, dentro de los parentesis se pone tal cual en css
+
+//el text content lee el texto
+
+document.querySelector('.message').textContent = 'Correct Number!';// asi se cambia el texto;
+
+document.querySelector
+            
