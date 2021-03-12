49bf90a45cb080dcd5c197b9180a2a4f897ebde7
Natalie Acevedo <natalieacevedo25@gmail.com>
2021-03-12 12:44:18 +0100

    all funcionality before coding challenge reseting again button

--- guessMyNumber/script.js
+++ guessMyNumber/script.js
@@ -1,10 +1,71 @@
 'use strict';
-
+/*
 console.log(document.querySelector('.message').textContent);// selecciona el elemento, dentro de los parentesis se pone tal cual en css
 
 //el text content lee el texto
 
-document.querySelector('.message').textContent = 'Correct Number!';// asi se cambia el texto;
+document.querySelector('.message').textContent = 'Correctisimo Number!';// asi se cambia el texto;
 
-document.querySelector
-            
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 20;
+
+
+document.querySelector('.guess').value = 24;//se manipula el valor;
+
+console.log(document.querySelector('.guess').value);//lee el valor;
+
+*/
+const secret = Math.trunc(Math.random() * 20) + 1;
+
+let score = 20;
+
+
+
+
+
+
+
+//HANDLING CLICK EVENTS!!!!!!!!
+
+document.querySelector('.check').addEventListener('click', function () {
+    const guess = Number(document.querySelector('.guess').value);
+
+    //when there is no input
+    if (!guess) {
+        document.querySelector('.message').textContent = 'please put a valid input'
+    }
+
+    //when player wins the game
+    else if (secret === guess) {
+        document.querySelector('.number').textContent = secret;
+        document.querySelector('.message').textContent = 'You are correct'
+        //asi se cambia style de css :)
+        document.querySelector('body').style.backgroundColor = '#60b347';
+        document.querySelector('.number').style.width = '40rem';
+        
+        //when input is higher than secret number
+    }else if (guess > secret) {
+        if (score > 1) { 
+        document.querySelector('.message').textContent = 'Too high, please try again';
+        score--;
+        document.querySelector('.score').textContent = score;
+        } else {
+            document.querySelector('.message').textContent = 'You lost the game';
+            document.querySelector('.score').textContent = 0;
+    }
+        //when input is lower than secret number
+    } else if (guess < secret) {
+        if (score > 1) {
+            document.querySelector('.message').textContent = 'Too low, please try again';
+            score--;
+            document.querySelector('.score').textContent = score;
+        } else {
+            document.querySelector('.message').textContent = 'You lost the game';
+            document.querySelector('.score').textContent = 0;
+        }
+           
+    };
+    
+});
+
+ //el event listener toma como segundo argumento una funcion en donde uno le dice que quiere que pase cuando se clickea o lo que sea el evento
