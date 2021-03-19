bee72a2aae352ca2dd35e61ebf67da9a06a2962f
Natalie Acevedo <natalieacevedo25@gmail.com>
2021-03-19 12:18:53 +0100

    I finished the modelWindow little project

--- modalWindow/script.js
+++ modalWindow/script.js
@@ -8,6 +8,8 @@ const btnCloseModal = document.querySelector('.close-modal');
 
 const tresBotones = document.querySelectorAll('.show-modal');
 
+const hidden = document.querySelector('hidden');
+
 let fromtresBotonesToArray = Array.from(tresBotones);// lo converti a array
 
 
@@ -36,3 +38,28 @@ overlay.addEventListener('click', closing);
    console.log(tresBotones[i].textContent);
 }
 */
+
+//HERE WE ARE LISTENING EVENTS EVERYWHERE IN THE PAGE, NOTE WE DIDNT SPECIFY A CLASS!
+//NO MATTER WHEN IT HAPPENS IN THE PAGE, IS GONNA GET TRIGGER!!
+
+//el keydown es cuando uno quita el dedo del teclado //devulve un objeto cuando el keydown pasa
+
+
+/* MI SOLUCION QUE ME SALIO BIEN SIN CLASS LIST CREANDO UNA CONST PARA HIDDEN
+document.addEventListener('keydown', (event) => {
+    console.log(event);
+    if (event['key'] === 'Escape' && !hidden) {
+        closing();
+
+    };
+        
+});
+
+*/
+//LA SOLUCION DEL PROFE CON CLASSLIST:
+
+document.addEventListener('keydown', (event) => {
+    console.log(event.key);
+    event.key === 'Escape' && !modal.classList.contains('hidden') ? closing() : console.log('press something else');
+       
+    });
