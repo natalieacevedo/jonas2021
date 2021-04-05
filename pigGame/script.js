a06ac30eca838152e1aa85b847622be064ad445c
Natalie Acevedo <natalieacevedo25@gmail.com>
2021-04-05 23:46:03 +0200

    Finally the hold button works

--- pigGame/script.js
+++ pigGame/script.js
@@ -2,7 +2,7 @@
 
 const newGameButton = document.querySelector('.btn--new');
 const acumulatorPoints = Array.from(document.querySelectorAll('.score'));//array type con los dos puntajes
-const currentScore = Array.from(document.querySelectorAll('.current-score'))//aqui tambien;
+const currentScore = document.querySelectorAll('.current-score')//aqui tambien;
 const rollingDiceButton = document.querySelector('.btn--roll');
 const holdButton = document.querySelector('.btn--hold');
 const dices = document.querySelector('.dice');
@@ -15,10 +15,13 @@ const bothCurrentsWithClass = document.querySelectorAll('.current-score');
 
 
 
+
+
 //let randomDiceNumber = Math.trunc(Math.random() * 6) + 1;
-let inicio = 0;
+
 let acumulaCurrent = 0;
 let activePlayer = 0;
+let finalazo = [0, 0];
 
 newGameAZero(dices, acumulatorPoints, currentScore);
 
@@ -26,7 +29,7 @@ newGameAZero(dices, acumulatorPoints, currentScore);
 
     function newGameAZero(dados,...args) {
  
-     args.forEach(ele => ele.forEach(el => el.textContent = inicio));
+     args.forEach(ele => ele.forEach(el => el.textContent = 0));
     dados.classList.add('hidden');
 };
 
@@ -45,23 +48,49 @@ function rollingDados() {
        
         acumulaCurrent += randomDiceNumber;
         document.getElementById(`current--${activePlayer}`).textContent = acumulaCurrent;
-       // document.querySelector(`.player--${activePlayer}`).classList.add('player--active');
+    
     } else {
-       // document.querySelector(`.player--${activePlayer}`).classList.remove('player--active');
+      
         acumulaCurrent = 0
         document.getElementById(`current--${activePlayer}`).textContent = acumulaCurrent;
         activePlayer = activePlayer === 0 ?  1 : 0;
         document.querySelector(`.player--0`).classList.toggle('player--active');
         document.querySelector(`.player--1`).classList.toggle('player--active');
-        console.log(document.querySelector(`.player--${activePlayer}`))
+       // console.log(document.querySelector(`.player--${activePlayer}`))
    }
   
 }
 
 //Ask fide why does not work when putting the parameters in the function
 newGameButton.addEventListener('click', () => {
+   
+    document.querySelector(`.player--0`).classList.add('player--active');
+    document.querySelector(`.player--1`).classList.remove('player--active');
+   
+    acumulaCurrent = 0;
+   // activePlayer = activePlayer === 1 ? 0 : 1;
+    activePlayer = 0;
     newGameAZero(dices, currentScore, acumulatorPoints);
+    finalazo = [0, 0];
+    
 });
 
 
 rollingDiceButton.addEventListener('click', rollingDados);
+
+
+holdButton.addEventListener('click', () => {
+   
+    finalazo[activePlayer] += acumulaCurrent;
+    document.getElementById(`score--${activePlayer}`).textContent = finalazo[activePlayer];
+    acumulaCurrent = 0;
+    document.getElementById(`current--${activePlayer}`).textContent = acumulaCurrent;
+    document.querySelector(`.player--0`).classList.toggle('player--active');
+    document.querySelector(`.player--1`).classList.toggle('player--active');
+
+
+    activePlayer = activePlayer === 0 ? 1 : 0;
+   
+});
+
+
